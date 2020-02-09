c41253bbfc2d337d17cf0b792fe91f3f9235a386
Yukti <yukti@sarthaks-MacBook-Air.local>
2020-02-09 22:44:49 +0530

    basic layout

---
+++ WrdInspection/app/src/main/java/com/gdg/bhopal/wrdinspection/Thanku.java
@@ -0,0 +1,14 @@
+package com.gdg.bhopal.wrdinspection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Thanku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_thanku );
+    }
+}

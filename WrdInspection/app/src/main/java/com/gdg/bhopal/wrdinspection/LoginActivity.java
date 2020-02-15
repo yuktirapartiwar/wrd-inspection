98120014d194d023ce60f6912d7232527ebd38ec
yukti <1599.yukti@gmail.com>
2020-02-15 21:18:56 +0530

    login

--- WrdInspection/app/src/main/java/com/gdg/bhopal/wrdinspection/LoginActivity.java
+++ WrdInspection/app/src/main/java/com/gdg/bhopal/wrdinspection/LoginActivity.java
@@ -1,26 +1,52 @@
 package com.gdg.bhopal.wrdinspection;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ProgressDialog loadingBar;
+    TextView username;
+    TextView password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        loadingBar = new ProgressDialog(this);
+        username =  findViewById(R.id.usernameTxt);
+        password =  findViewById(R.id.passwordTxt);
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
         Button loginButton = findViewById( R.id.loginBtn );
         loginButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent( LoginActivity.this , MainMenu.class );
-                startActivity(intent);
+
+//                String u = username.getText().toString().trim();
+//                String p = password.getText().toString();
+//                if (u.trim().equals("") || p.trim().equals("")) {
+//                    Toast.makeText(LoginActivity.this, "Fields are empty", Toast.LENGTH_LONG).show();
+//
+//                } else {
+
+                    loadingBar.setTitle( "Login" );
+                    loadingBar.setMessage( "Please wait, while we are checking the credentials." );
+                    loadingBar.setCanceledOnTouchOutside( false );
+                    loadingBar.show();
+
+
+                    Intent intent = new Intent( LoginActivity.this, MainMenu.class );
+                    startActivity( intent );
+  //              }
     }
 });
     }
